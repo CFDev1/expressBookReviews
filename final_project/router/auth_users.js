153cdb2e134886f38bf8fd948e449f67bcbdcf18
CFDev1 <151486542+CFDev1@users.noreply.github.com>
2024-04-16 23:05:10 -0400

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,21 +6,53 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-router.get("/",(req,res)=>{
-
- router.get("/",(req,res)=>{
-    res.send(JSON.stringify(friends,null,4));
+ let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+redgd_users.post("/login",(req,res)=>{
+const username = req.body.username;
+const password = req.body.password;
+if (username && password) {
+  if (!doesExist(username)) {
+    users.push({"username":username,"password":password});
+    return res.status(200).json({message: "User successfully registred. Now you can login"});
+  } else {
+    return res.status(404).json({message: "User already exists!"});
+  }
+}
+return res.status(404).json({message: "Unable to register user."});
 });
+
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+      
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
